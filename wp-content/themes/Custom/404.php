cbb3f1a9e9f5723c432359c9632b168f88c0e10a
Andre <andre02.9d@gmail.com>
2017-09-12 15:59:06 +0700

    Merubah pesan not found

--- wp-content/themes/Custom/404.php
+++ wp-content/themes/Custom/404.php
@@ -2,8 +2,18 @@
 get_header();?>
 <div class="page-404">
 	<div class="container text-center">
-		<h2 class="title-404">404 Not Found!!!</h2>
-		<p class="descript">Sorry! Not Found</p>
+		<h2 class="title-404">Yahhhhh !!!</h2>
+		<p class="descript">Kamu Cari APAAN SIHH ?!! <br> Ga Ketemu Nih Balik aja 
+		                        <br>
+                            <a href="/wordpress">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-home fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        
+		
+		
 	</div>
 </div>
 <?php
